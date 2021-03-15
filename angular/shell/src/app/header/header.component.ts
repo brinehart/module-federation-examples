1b9cb777b7ea6f7ee44f48e3701caaa2b4f63dc7
brinehart <ben@brinehartdesign.com>
2021-03-15 16:37:20 -0400

    Add dummy links for 3 apps

--- angular/shell/src/app/header/header.component.ts
+++ angular/shell/src/app/header/header.component.ts
@@ -1,6 +1,11 @@
 import { Component, HostBinding, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
+export interface MenuItem {
+  label: string;
+  link: string;
+}
+
 @Component({
   selector: 'ff-header',
   templateUrl: './header.component.html',
@@ -11,6 +16,21 @@ export class HeaderComponent implements OnInit {
   siteTitle = 'Final Fantasy Bonanza';
 
   toggleControl = new FormControl(false);
+
+  navigation: MenuItem[] = [
+    {
+      label: 'Final Fantasy VII',
+      link: 'ff-vii'
+    },
+    {
+      label: 'Final Fantasy VIII',
+      link: 'ff-viii'
+    },
+    {
+      label: 'Final Fantasy IX',
+      link: 'ff-ix'
+    }
+  ]
 
   constructor() { }
 
